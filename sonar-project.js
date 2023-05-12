39f75ee057f1e156468f62d8f8d1d5ac30e58b60
mogazor <122966642+mogazor@users.noreply.github.com>
2023-05-12 19:44:07 +0000

    Updated with  sonarqube url and login key

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://23.22.244.182:8000/',
+  serverUrl: 'http://172.31.22.231:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
