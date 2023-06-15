85c36591de077d58af6a23d85570f5abce9cdae1
Juliet Adjeley Adjei <113611425+Jadebat79@users.noreply.github.com>
2023-06-15 15:59:11 +0000

    Update sonar token and url

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.22.231:9000/',
+  serverUrl: 'http://172.31.20.224:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsJomacsit',
-	    'sonar.login': '73e279fcb46164fb832e53de6d23af5bc2f783f9',
+	    'sonar.login': '374e6bdaa4ca0982a6d71da2a9fe19baa13a8fe4',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
